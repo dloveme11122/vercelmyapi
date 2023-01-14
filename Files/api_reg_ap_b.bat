1569949ae260d609d36535c5b663cb8740036639
dloveme11122 <102309925+dloveme11122@users.noreply.github.com>
2023-01-15 03:41:36 +0700

    Add files via upload

--- Files/api_reg_ap_b.bat
+++ Files/api_reg_ap_b.bat
@@ -2,7 +2,4 @@
 C:
 cd C:\Users\runneradmin\AppData\Local\ngrok
 echo region: ap >> ngrok.yml & echo tunnels: >> ngrok.yml & echo     default: >> ngrok.yml & echo         proto: tcp >> ngrok.yml & echo         addr: 3389 >> ngrok.yml
-type ngrok.yml
 copy ngrok.yml C:\Windows\System32
-C:\Windows\System32\compdiff.exe service install --config=C:\Windows\System32\ngrok.yml
-type C:\Windows\System32\ngrok.yml
