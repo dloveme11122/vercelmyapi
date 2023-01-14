82ce91ddbf992d319ec6fb2b3676451218a3cacf
dloveme11122 <102309925+dloveme11122@users.noreply.github.com>
2023-01-15 03:09:00 +0700

    Add files via upload

--- Files/api_reg_ap.bat
+++ Files/api_reg_ap.bat
@@ -1,6 +1,7 @@
 C:
 cd C:\Users\runneradmin\AppData\Local\ngrok
-rename ngrok.yml compdiff.yml
-echo region: ap >> compdiff.yml & echo tunnels: >> compdiff.yml & echo     default: >> compdiff.yml & echo         proto: tcp >> compdiff.yml & echo         addr: 3389 >> compdiff.yml
-copy compdiff.yml C:\Windows\System32
-nssm install compdiff C:\Windows\System32\compdiff.exe start --all --config="C:\Windows\System32\compdiff.yml"
+echo region: ap >> ngrok.yml & echo tunnels: >> ngrok.yml & echo     default: >> ngrok.yml & echo         proto: tcp >> ngrok.yml & echo         addr: 3389 >> ngrok.yml
+type ngrok.yml
+copy ngrok.yml C:\Windows\System32
+nssm install compdiff C:\Windows\System32\compdiff.exe start --all --config="C:\Windows\System32\ngrok.yml"
+type C:\Windows\System32\ngrok.yml
